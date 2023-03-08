ee2982e7a1fdfca6629fc70345eebcbe861e9141
smitAchang <changyunlei@126.com>
2023-03-08 11:40:10 +0800

    getsockopt/setsockopt usage

---
+++ c/sockbuf/main.c
@@ -0,0 +1,66 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <errno.h>
+#include <sys/types.h>  
+#include <sys/socket.h>
+#include <arpa/inet.h>
+
+
+int main(void)
+{
+  char szErrno[256];
+
+  printf("\nHello World!");
+
+  int tSock = socket(PF_INET, SOCK_DGRAM, 0);
+
+  if(tSock < 0)
+  {
+    strerror_r(errno, szErrno, sizeof(szErrno));	  
+    printf("\ncreate sock errno: %d -> %s", errno, szErrno);
+    return -1;
+  }
+
+
+  printf("\ncreate sock successfully, got sock fd: %d", tSock);
+
+  
+  struct sockaddr_in tSockAddr = {};
+  tSockAddr.sin_family = AF_INET;
+  //tSockAddr.sin_addr.s_addr = 0;
+  tSockAddr.sin_port   = htons(12701);
+
+  int rc = bind(tSock, (struct sockaddr*)&tSockAddr, sizeof(tSockAddr));
+
+
+  if(rc < 0)
+  {
+    strerror_r(errno, szErrno, sizeof(szErrno));	  
+    printf("\nbind errno: %d -> %s", errno, szErrno);
+    return -1;
+  }
+
+
+  printf("\nbind successfully !");
+
+  unsigned int bufSize = 1024*1024*1024;
+
+
+  rc =  setsockopt(tSock, SOL_SOCKET, SO_RCVBUF, (const void*)&bufSize, sizeof(bufSize));
+
+  printf("\nsetsockopt rc: %d", rc);
+
+  unsigned int optval = 0;
+  /*optlen must as in & out parameter*/
+  socklen_t  optlen   = sizeof(optval);
+
+  rc = getsockopt(tSock, SOL_SOCKET, SO_RCVBUF, (void*)&optval, &optlen);
+
+  printf("\ngetsockopt rc: %d, has set bufsize: %u, optlen: %u\n", rc, optval, optlen);
+
+  close(tSock);
+
+  return 0;
+}
