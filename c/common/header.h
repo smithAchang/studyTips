08bd738ee77682f19af1dc1f5d987f5f529087cf
smitAchang <changyunlei@126.com>
2023-04-03 18:01:39 +0800

    test udp checksum

--- c/common/header.h
+++ c/common/header.h
@@ -1,7 +1,9 @@
 #ifndef _MY_HEADER_H
 #define _MY_HEADER_H
 
+#include <stddef.h>
 #include <stdint.h>
+#include <arpa/inet.h>
 
 #ifdef __cplusplus
 extern "C" {
@@ -9,6 +11,12 @@ extern "C" {
 
 #pragma pack(push)
 #pragma pack(1)
+
+enum IPTYPES
+{
+   IPTYPE_V4 = 0x4,
+   IPTYPE_V6 = 0x6
+};
 
 typedef struct _IP_HEADER_t
 {
@@ -20,9 +28,19 @@ typedef struct _IP_HEADER_t
    uint8_t  bTTL;                 // 存活时间
    uint8_t  bTypeOfProtocol;     // 协议类型
    uint16_t u16CheckSum;        // 校验和
-   uint32_t u32SourIp;         // 源ip
-   uint32_t u32DestIp;        // 目的ip
-} IP_HEADER_t, *IP_HEADER_t_Ptr;
+   union
+   {
+      uint8_t  abSrcAddr[4];
+      uint32_t u32SourIp;         // 源ip   
+   };
+   
+   union
+   {
+      uint8_t  abDstAddr[4];
+      uint32_t u32DestIp;        // 目的ip   
+   };
+   
+} IP_HEADER_t, IPv4_HEADER_t, *IP_HEADER_t_Ptr, *IPv4_HEADER_t_Ptr;
 
 typedef struct _IPv6_HEADER_t
 {
@@ -80,10 +98,10 @@ static uint16_t csum (const uint16_t *packet, int packlen) {
    while (sum >> 16)
       sum = (sum & 0xffff) + (sum >> 16);
 
-   return (u_int16_t) ~sum;
+   return (uint16_t) ~sum;
 }
 
-static void udp4_header_checksum(IPv4_HEADER_t_Ptr pIPv4Header, UDP_HEADER_t_Ptr ptUDPHeader, size_t udpLen/*included header*/)
+static void udp4_header_checksum2(IPv4_HEADER_t_Ptr pIPv4Header, UDP_HEADER_t_Ptr ptUDPHeader, size_t udpLen/*included header*/)
 {
    uint32_t sum = 0;
 
@@ -93,21 +111,21 @@ static void udp4_header_checksum(IPv4_HEADER_t_Ptr pIPv4Header, UDP_HEADER_t_Ptr
    ptUDPHeader->u16Length = htons(udpLen);
    sum += udpLen;
 
-   sum  = Checksum((uint16_t *)&pIPv4Header->abSrcAddr, 16);
-   sum  = Checksum((uint16_t *)&pIPv4Header->abDstAddr, 16);
+   sum  = csum((uint16_t *)&pIPv4Header->abSrcAddr, 4);
+   sum  = csum((uint16_t *)&pIPv4Header->abDstAddr, 4);
 
    sum += ((uint16_t)pIPv4Header->bTypeOfProtocol & 0x00FF);
    //finish the pseudo header checksum
 
    //udp section
    ptUDPHeader->u16CheckSum = 0;
-   sum = Checksum((uint16_t *)ptUDPHeader, udpLen);
+   sum = csum((uint16_t *)ptUDPHeader, udpLen);
 
    ptUDPHeader->u16CheckSum = htons((uint16_t)(~sum));
 }
 
 
-static void udp6_header_checksum(IPv6_HEADER_t_Ptr pIPv6Header, UDP_HEADER_t_Ptr ptUDPHeader, size_t udpLen/*included header*/)
+static void udp6_header_checksum2(IPv6_HEADER_t_Ptr pIPv6Header, UDP_HEADER_t_Ptr ptUDPHeader, size_t udpLen/*included header*/)
 {
    uint32_t sum = 0;
 
@@ -115,17 +133,107 @@ static void udp6_header_checksum(IPv6_HEADER_t_Ptr pIPv6Header, UDP_HEADER_t_Ptr
    ptUDPHeader->u16Length = htons(udpLen);
    sum += udpLen;
 
-   sum  = Checksum((uint16_t *)&pIPv6Header->abSrcAddr, 16);
-   sum  = Checksum((uint16_t *)&pIPv6Header->abDstAddr, 16);
+   sum  = csum((uint16_t *)&pIPv6Header->abSrcAddr, 16);
+   sum  = csum((uint16_t *)&pIPv6Header->abDstAddr, 16);
 
    sum += ((uint16_t)pIPv6Header->bNextHeader & 0x00FF);
     //finish the pseudo header checksum
 
    //udp section
    ptUDPHeader->u16CheckSum = 0;
-   sum = Checksum((uint16_t *)ptUDPHeader, udpLen);
+   sum = csum((uint16_t *)ptUDPHeader, udpLen);
 
    ptUDPHeader->u16CheckSum = htons((uint16_t)(~sum));
+}
+
+
+uint32_t Checksum(uint32_t cksum, const uint8_t *pBuffer, uint32_t size)
+{
+
+  if ((NULL == pBuffer) || (0 == size))
+  {
+      // return passed value
+      return cksum;
+  }
+
+  uint32_t num = 0;
+  const uint8_t *p   = pBuffer;
+
+  while (size > 1)
+  {
+      cksum += ((uint16_t)p[num] << 8 & 0xff00) | ((uint16_t)p[num + 1] & 0x00FF);
+      size  -= 2;
+      num   += 2;
+  }
+
+  if (size > 0)
+  {
+    cksum += ((uint16_t)p[num] << 8) & 0xFFFF;
+    num += 1;
+  }
+
+  while (cksum >> 16)
+  {
+    cksum = (cksum & 0xFFFF) + (cksum >> 16);
+  }
+
+  return cksum;
+}
+
+void ipv4_header_checksum(IPv4_HEADER_t_Ptr pIpHeader)
+{
+   uint8_t ipHeadLen      = (pIpHeader->bVersionAndHeaderLen & 0x0F) << 2;
+
+   pIpHeader->u16CheckSum = 0;
+
+   uint32_t sum = Checksum(0, (uint8_t *)pIpHeader, ipHeadLen);
+
+   pIpHeader->u16CheckSum = htons((uint16_t)(~sum));
+
+}
+
+void udp4_header_checksum(IPv4_HEADER_t_Ptr ptIPv4Header, UDP_HEADER_t_Ptr ptUDPHeader, size_t udpLen/*include header*/)
+{
+   uint32_t sum = 0;
+
+   //
+   ptUDPHeader->u16Length = htons(udpLen);
+   sum += udpLen;
+
+   sum  = Checksum(sum, (uint8_t *)&ptIPv4Header->abSrcAddr, 4);
+   sum  = Checksum(sum, (uint8_t *)&ptIPv4Header->abDstAddr, 4);
+
+   sum += ((uint16_t)ptIPv4Header->bTypeOfProtocol & 0x00FF);
+   //finish the pseudo header checksum
+
+   //udp section
+   ptUDPHeader->u16CheckSum = 0;
+   sum = Checksum(sum, (uint8_t *)ptUDPHeader, udpLen);
+
+   ptUDPHeader->u16CheckSum = htons((uint16_t)(~sum));
+
+}
+
+void udp6_header_checksum(IPv6_HEADER_t_Ptr pIpHeader, UDP_HEADER_t_Ptr ptUDPHeader, size_t udpLen/*include header*/)
+{
+   uint32_t sum = 0;
+
+   //
+   ptUDPHeader->u16Length = htons(udpLen);
+   sum += udpLen;
+
+   sum  = Checksum(sum, (uint8_t *)&pIpHeader->abSrcAddr, 16);
+   sum  = Checksum(sum, (uint8_t *)&pIpHeader->abDstAddr, 16);
+
+   sum += ((uint16_t)pIpHeader->bNextHeader & 0x00FF);
+   //finish the pseudo header checksum
+
+   //udp section
+   ptUDPHeader->u16CheckSum = 0;
+   sum = Checksum(sum, (uint8_t *)ptUDPHeader, udpLen);
+
+   ptUDPHeader->u16CheckSum = htons((uint16_t)(~sum));
+
 }
 
 #ifdef __cplusplus
--- c/raw6/rawudp.c
+++ c/raw6/rawudp.c
@@ -9,12 +9,15 @@
 
 #include "header.h"
 
+void sendV6(int tRawUDPSock, struct sockaddr_in6 from, struct sockaddr_in6 to, int iovec_flag, uint16_t  u16ContentLen);
+void sendV4(int tRawUDPSock, struct sockaddr_in from,  struct sockaddr_in to, int iovec_flag,  uint16_t  u16ContentLen);
 
 int main(int argc , char* argv[])
 {
   char szErrno[256];
   char    szFromAddrBuf[64] = {};
   char    szToAddrBuf[64]   = {};
+  int rc;
 
   printf("Hello World!\n");
   struct sockaddr_in6 from = {}, to = {};
@@ -22,28 +25,39 @@ int main(int argc , char* argv[])
   inet_pton(PF_INET6, "FEC0:2021::192.168.31.1", &from.sin6_addr);
   inet_pton(PF_INET6, "FEC0:2021::192.168.31.2", &to.sin6_addr);
 
-  int iovec_flag, opt;
+  struct sockaddr_in from4 = {}, to4 = {};
+  from4.sin_family = to4.sin_family = AF_INET;
+  inet_pton(PF_INET, "192.168.31.1", &from4.sin_addr);
+  inet_pton(PF_INET, "192.168.31.2", &to4.sin_addr);
+
+  int iovec_flag,  opt;
+  enum IPTYPES ip_flag = IPTYPE_V6;
   uint16_t  u16ContentLen = 2049;
   iovec_flag = 0;
 
-  while ((opt = getopt(argc, argv, "vs:d:l:")) != -1) {
+  while ((opt = getopt(argc, argv, "4vs:d:l:")) != -1) {
      switch (opt) {
      case 'v':
          iovec_flag = 1;
          break;
      case 's':
-          printf("change src addr to %s\n", optarg);
+          printf("Change src addr to %s\n", optarg);
          inet_pton(PF_INET6, optarg, &from.sin6_addr);
+         inet_pton(PF_INET,  optarg, &from4.sin_addr);
+         break;
+     case 'd':
+         printf("Change dst addr to %s\n", optarg);
+         inet_pton(PF_INET6, optarg, &to.sin6_addr);
+         inet_pton(PF_INET,  optarg, &to4.sin_addr);
          break;
      case 'l':
          u16ContentLen = atoi(optarg);        
          break;
-     case 'd':
-         printf("change dst addr to %s\n", optarg);
-         inet_pton(PF_INET6, optarg, &to.sin6_addr);
+     case '4':
+         ip_flag = IPTYPE_V4;        
          break;
      default: /* '?' */
-         fprintf(stderr, "Usage: %s [-s src] [-d dst] [-l len] [-v] name\n",
+         fprintf(stderr, "Usage: %s [-s src] [-d dst] [-l len] [-v] [-4] name\n",
                  argv[0]);
          return 1;
      }
@@ -53,41 +67,84 @@ int main(int argc , char* argv[])
   inet_ntop(AF_INET6, &from.sin6_addr, szFromAddrBuf, sizeof(szFromAddrBuf));
   inet_ntop(AF_INET6, &to.sin6_addr,   szToAddrBuf, sizeof(szToAddrBuf));
 
-  printf("src=%s; dst=%s; u16ContentLen=%u; iovec_flag=%d; optind=%d\n",
-         szFromAddrBuf, szToAddrBuf, u16ContentLen, iovec_flag, optind);
+  if(ip_flag == IPTYPE_V4)
+  {
+    inet_ntop(AF_INET, &from4.sin_addr, szFromAddrBuf, sizeof(szFromAddrBuf));
+    inet_ntop(AF_INET, &to4.sin_addr,   szToAddrBuf, sizeof(szToAddrBuf));
+  }
+
+  printf("ip_flag=%d; Src=%s; dst=%s; u16ContentLen=%u; iovec_flag=%d; optind=%d\n",
+         ip_flag, szFromAddrBuf, szToAddrBuf, u16ContentLen, iovec_flag, optind);
 
 
   if (optind >= argc) {
-     fprintf(stderr, "can Expected argument after options\n");
+     fprintf(stderr, "Can Expected argument after options\n");
   }
 
+  int tRawUDPSock;
 
-  int tRawUDPSock = socket(PF_INET6, SOCK_RAW, IPPROTO_UDP);
+  if(ip_flag == IPTYPE_V6)
+  {
+    tRawUDPSock = socket(PF_INET6, SOCK_RAW, IPPROTO_UDP);
+  }
+  else
+  {
+    tRawUDPSock = socket(PF_INET, SOCK_RAW, IPPROTO_UDP);
+  }
 
   if(tRawUDPSock < 0)
   {
     strerror_r(errno, szErrno, sizeof(szErrno));	  
-    printf("create raw udp protocol sock errno: %d -> %s\n", errno, szErrno);
+    printf("Create raw udp protocol sock errno: %d -> %s\n", errno, szErrno);
     return -1;
   }
 
-  printf("create raw udp protocol sock successfully, got sock fd: %d\n", tRawUDPSock);
+  printf("Create raw udp protocol sock successfully, got sock fd: %d\n", tRawUDPSock);
+
+  if(ip_flag == IPTYPE_V6)
+  {
+    rc = bind(tRawUDPSock, (struct sockaddr*)&from, sizeof(from));
+  }
+  else
+  {
+    rc = bind(tRawUDPSock, (struct sockaddr*)&from4, sizeof(from4));
+  }
 
    
-  //old bugs
-  //from.sin6_port = htons(IPPROTO_UDP);
-  int rc = bind(tRawUDPSock, (struct sockaddr*)&from, sizeof(from));
   if(rc < 0)
   {
     strerror_r(errno, szErrno, sizeof(szErrno));    
-    printf("bind addr: %s , errno: %d -> %s\n", szFromAddrBuf, errno, szErrno);
+    printf("Bind addr: %s , errno: %d -> %s\n", szFromAddrBuf, errno, szErrno);
 
     close(tRawUDPSock);
     return -1;
   }
-  printf("bind successfully !\n");
+  printf("Bind successfully !\n");
 
-  uint8_t abPktInfo[4096] = {};
+  
+  if(ip_flag == IPTYPE_V6)
+  {
+    sendV6(tRawUDPSock, from, to, iovec_flag, u16ContentLen);
+  }
+  else
+  {
+    sendV4(tRawUDPSock, from4, to4, iovec_flag, u16ContentLen);
+  }
+  
+  
+  
+  printf("Test program exit!\n"); 
+  close(tRawUDPSock);
+
+  return 0;
+}
+
+static uint8_t abPktInfo[4096];
+
+void sendV6(int tRawUDPSock, struct sockaddr_in6 from, struct sockaddr_in6 to, int iovec_flag, uint16_t  u16ContentLen)
+{
+  char szErrno[256];
+
  
   from.sin6_port = 0;
   to.sin6_port   = 0;
@@ -104,13 +161,16 @@ int main(int argc , char* argv[])
 
   UDP_HEADER_t* ptUdpHdr = (UDP_HEADER_t*)(abPktInfo + sizeof(IPv6_HEADER_t));
 
+  size_t reqLen = sizeof(UDP_HEADER_t) + u16ContentLen;
   ptUdpHdr->u16SrcPort   = htons(12760);    
   ptUdpHdr->u16DstPort   = htons(22760);   
-  ptUdpHdr->u16Length    = htons(sizeof(UDP_HEADER_t) + u16ContentLen);   
+  ptUdpHdr->u16Length    = htons(reqLen);   
   ptUdpHdr->u16CheckSum  = 0;
+  udp6_header_checksum(ptIPv6_header, ptUdpHdr, reqLen);
 
-  size_t reqLen = sizeof(UDP_HEADER_t) + u16ContentLen;
-  printf("will send %zu bytes\n", reqLen);
+  
+  printf("Will send IPv6 %zu bytes\n", reqLen);
+
   ssize_t sendLen ;
   if(iovec_flag)
   {
@@ -122,7 +182,9 @@ int main(int argc , char* argv[])
     udpvec[0].iov_len  = sizeof(UDP_HEADER_t);
     udpvec[1].iov_base = "hello world";
     udpvec[1].iov_len  = sizeof("hello world");
-    ptUdpHdr->u16Length    = htons(sizeof(UDP_HEADER_t) + udpvec[1].iov_len);   
+    reqLen             = sizeof(UDP_HEADER_t) + udpvec[1].iov_len;
+    ptUdpHdr->u16Length    = htons(reqLen);
+    udp6_header_checksum(ptIPv6_header, ptUdpHdr, reqLen);   
 
     struct msghdr msghdr  = {};
     msghdr.msg_name       = (void*)&iovec_to;       /* Optional address */
@@ -138,7 +200,7 @@ int main(int argc , char* argv[])
     
     if (sendLen < 0) {
      strerror_r(errno, szErrno, sizeof(szErrno));   
-     fprintf(stderr, "send fail when sin6_port is not zero; errno:%d -> %s\n", errno, szErrno);
+     fprintf(stderr, "Send fail when sin6_port is not zero; errno:%d -> %s\n", errno, szErrno);
     }
 
     iovec_to.sin6_port           = 0;
@@ -151,18 +213,94 @@ int main(int argc , char* argv[])
  
   if(sendLen < 0)
   {
-    strerror_r(errno, szErrno, sizeof(szErrno));	  
-    printf("send data errno: %d -> %s\n", errno, szErrno);
+    strerror_r(errno, szErrno, sizeof(szErrno));    
+    printf("Send data errno: %d -> %s\n", errno, szErrno);
   }
   else
   {
-    printf("has send data %zd bytes!\n", sendLen); 
+    printf("Has send data %zd bytes!\n", sendLen); 
   }
-  
-  
-  
-  printf("test program exit!\n"); 
-  close(tRawUDPSock);
 
-  return 0;
+}
+
+void sendV4(int tRawUDPSock, struct sockaddr_in from, struct sockaddr_in to, int iovec_flag, uint16_t  u16ContentLen)
+{
+  char szErrno[256];
+
+ 
+  from.sin_port = 0;
+  to.sin_port   = 0;
+
+  IPv4_HEADER_t* ptIPv4_header = (IPv4_HEADER_t*)abPktInfo;
+  memset(ptIPv4_header, 0, sizeof(*ptIPv4_header));
+
+  ptIPv4_header->bVersionAndHeaderLen = 0x45;
+  ptIPv4_header->u16TotalLenOfPacket = htons(sizeof(IPv4_HEADER_t) + sizeof(UDP_HEADER_t) + u16ContentLen); 
+  ptIPv4_header->bTypeOfProtocol     = IPPROTO_UDP; 
+  ptIPv4_header->bTTL                = 128; 
+  memcpy(ptIPv4_header->abSrcAddr, &from.sin_addr, sizeof(from.sin_addr));
+  memcpy(ptIPv4_header->abDstAddr, &to.sin_addr, sizeof(to.sin_addr));
+
+  UDP_HEADER_t* ptUdpHdr = (UDP_HEADER_t*)(abPktInfo + sizeof(IPv4_HEADER_t));
+
+  size_t reqLen          = sizeof(UDP_HEADER_t) + u16ContentLen;
+  ptUdpHdr->u16SrcPort   = htons(12760);    
+  ptUdpHdr->u16DstPort   = htons(22760);   
+  ptUdpHdr->u16Length    = htons(reqLen);   
+  ptUdpHdr->u16CheckSum  = 0;
+  udp4_header_checksum(ptIPv4_header, ptUdpHdr, reqLen);
+
+  
+  printf("Will send IPv4 %zu bytes\n", reqLen);
+
+  ssize_t sendLen ;
+  if(iovec_flag)
+  {
+    struct sockaddr_in  iovec_to = to;
+    iovec_to.sin_port            = 1234;
+
+    struct iovec udpvec[2];
+    udpvec[0].iov_base = ptUdpHdr;
+    udpvec[0].iov_len  = sizeof(UDP_HEADER_t);
+    udpvec[1].iov_base = "hello world";
+    udpvec[1].iov_len  = sizeof("hello world");
+    reqLen             = sizeof(UDP_HEADER_t) + udpvec[1].iov_len;
+    ptUdpHdr->u16Length    = htons(reqLen);
+    udp4_header_checksum(ptIPv4_header, ptUdpHdr, reqLen);   
+
+    struct msghdr msghdr  = {};
+    msghdr.msg_name       = (void*)&iovec_to;       /* Optional address */
+    msghdr.msg_namelen    = sizeof(iovec_to);    /* Size of address */
+    msghdr.msg_iov        = udpvec;        /* Scatter/gather array */
+    msghdr.msg_iovlen     = sizeof(udpvec)/sizeof(udpvec[0]);     /* # elements in msg_iov */
+    msghdr.msg_control    = 0;    /* Ancillary data, see below */
+    msghdr.msg_controllen = 0; /* Ancillary data buffer len */
+    msghdr.msg_flags      = 0;      /* Flags (unused) */
+
+
+    sendLen = sendmsg(tRawUDPSock, &msghdr, 0/*flags*/);
+    
+    if (sendLen < 0) {
+     strerror_r(errno, szErrno, sizeof(szErrno));   
+     fprintf(stderr, "Send fail when sin6_port is not zero; errno:%d -> %s\n", errno, szErrno);
+    }
+
+    iovec_to.sin_port           = 0;
+    sendLen = sendmsg(tRawUDPSock, &msghdr, 0/*flags*/);
+  }
+  else
+  {
+    sendLen = sendto(tRawUDPSock, ptUdpHdr, reqLen, 0/*flags*/, (const struct sockaddr*)&to,  sizeof(to));
+  }
+ 
+  if(sendLen < 0)
+  {
+    strerror_r(errno, szErrno, sizeof(szErrno));    
+    printf("Send data errno: %d -> %s\n", errno, szErrno);
+  }
+  else
+  {
+    printf("Has send data %zd bytes!\n", sendLen); 
+  }
+
 }
