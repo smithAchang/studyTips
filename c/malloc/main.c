38f4b6a53a19c73097d612d54bf869f8f9d86c9b
SmithAchang <changyunlei@126.com>
2024-01-12 21:14:01 +0800

    增加malloc申请内存时的行为观察

---
+++ c/malloc/main.c
@@ -0,0 +1,61 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <errno.h>
+#include <limits.h>
+#include <stdint.h>
+
+
+int main(int argc , char* argv[])
+{
+  printf("Hello world!\n"
+         "To see the `top` command memory statistics!\n\n");
+
+  int sleeptime = 3;
+
+  if(argc > 2)
+  {
+    sleeptime = atoi(argv[1]);
+  }
+
+  printf("Using sleeptime: %d argument to watch the incresing RES(physical) statistic\n"
+         "But the VIRT statistic keeps unchanged after malloc\n\n", sleeptime);
+  
+  printf("You can see the program's VIRT&RES&%%MEM statistics using `top` command\n\n"
+         "The initial VIRT&RES statistics must be small\n"
+         "Enter any key to continue to malloc a big memory area\n");
+
+  int c = getchar();
+  const size_t nBigSize = 1*1024*1024*1024;
+  char* const pbBigMem = malloc(nBigSize);
+  if(pbBigMem == NULL)
+  {
+    perror("Malloc a big memory");
+    return -1;
+  }
+
+  printf("\nThe VIRT statistic is changed after malloc\n");
+
+  char* const pbBigMemEnd = pbBigMem + nBigSize;
+
+  char* cursor = pbBigMem;
+  do
+  {
+     // Access the memory to cause physical memory to be assigned
+     for(size_t i=0; i < (100*1024*1024) && cursor < pbBigMemEnd; ++i, ++cursor)
+      {
+        // one by one
+        cursor[0] = (char)i;
+      }  
+
+      printf("The RES statistic is changed after the memory is really scaned one by one, cursor: %p\n", cursor);
+      sleep(sleeptime);
+
+  }
+  while(cursor < pbBigMemEnd);
+
+
+  printf("\n\nExit from program !\n");
+  return 0;
+}
