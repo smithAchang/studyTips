06dd3405c1641762ce30d1714ba7b51d717b8e11
smitAchang <changyunlei@126.com>
2024-04-20 11:21:23 +0800

    makefile exercise

--- c/makefile/main.c
+++ c/makefile/main.c
@@ -5,6 +5,10 @@ extern int foo();
 
 int main(int argc , char* argv[])
 {
+   #ifdef _DEBUG
+   printf("DEBUG VERSION Running ...\n");  
+   #endif
+
    printf("foo: %d\n", foo());
    return 0;
 }
